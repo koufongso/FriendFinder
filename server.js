d74d8266ac594484b4517aa16665551797c010f4
Fung <koufongso@gmail.com>
2020-03-02 22:30:50 -0800

    setup server

---
+++ server.js
@@ -0,0 +1,17 @@
+// server setup
+var express = require("express");
+
+var server = express();
+var PORT = process.env.PORT || 8080;
+
+server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+
+// get routing function
+// require("./app/routing/apiRoutes")(server);
+// require("./app/routing/htmlRoutes")(server);
+
+// start the server
+server.listen(PORT,function(){
+    console.log("server is runing\n----------------------\n");
+});
