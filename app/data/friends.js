35424ad560406d2bb87fcf3314d8a413f29dcd6c
Fung <koufongso@gmail.com>
2020-03-08 15:08:14 -0700

    added a photo url to the dummy database for debug

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,7 +1,7 @@
 var frindes = [
     {
         "name": "Ahmed",
-        "photo": "",
+        "photo": "https://picsum.photos/id/237/200/300",
         "scores": [5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     },
 ]
